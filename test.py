b8189c873b804d2f9e245d6f28809073432168ae
Nicholas Chen <nicholasxchen@gmail.com>
2023-07-10 15:22:32 -0700

    begin FLASK
    flaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflaskflask

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("testtesttesttest")
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+@app.route("/pp")
+def home():
+  return "BRUH!"
+@app.route("/homie")
+def home2():
+  return render_template("homepage.html")
+if __name__ == "__main__":
+  app.run()
