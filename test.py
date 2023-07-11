ffdc042f8cd66406005a72615ea6449402afd789
Advaith Nair <64707611+adnx26@users.noreply.github.com>
2023-07-11 16:17:22 -0700

    Fixed Merging Issues

--- test.py
+++ test.py
@@ -2,16 +2,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
 @app.route("/")
 def home2():
-  return render_template("homepage.html")
-@app.route("/xob")
-def home3():
   return render_template("home.html")
-@app.route('/', methods =["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       first_name = request.form.get("fname")
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name +" "+ last_name
-    return render_template("form.html")
+@app.route("/form", methods =["GET"])
+def home3():
+  return render_template("form.html")
+
 if __name__ == "__main__":
   app.run()
