9603fce1062f253ad998b479a4d01e04d4348328
Nicholas Chen <nicholasxchen@gmail.com>
2023-07-11 16:06:05 -0700

    nav bar
    made a nav bar with icons

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
-@app.route("/pp")
-def home():
-  return "BRUH!"
-@app.route("/homie")
+@app.route("/")
 def home2():
   return render_template("homepage.html")
-@app.route('/', methods =["GET", "POST"])
+@app.route('/form', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
        first_name = request.form.get("fname")
