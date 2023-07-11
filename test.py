5b990ed0e8f0b81fa139388a9606c506ec0834cb
Nicholas Chen <nicholasxchen@gmail.com>
2023-07-11 13:02:51 -0700

    first form

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
-
-@app.route("/")
-def hello():
-  return "Hello World!"
 @app.route("/pp")
 def home():
   return "BRUH!"
 @app.route("/homie")
 def home2():
   return render_template("homepage.html")
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       first_name = request.form.get("fname")
+       last_name = request.form.get("lname")
+       return "Your name is "+first_name +" "+ last_name
+    return render_template("form.html")
 if __name__ == "__main__":
   app.run()
