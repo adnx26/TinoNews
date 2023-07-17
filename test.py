35bbd71ba4366658dcd25fdaa5bd6830e54cbe92
Nicholas Chen <nicholasxchen@gmail.com>
2023-07-17 15:13:11 -0700

    Re-written website

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
 @app.route("/")
 def home2():
   return render_template("home.html")
+
 @app.route("/AbtUs")
 def home1():
   return render_template("AboutUs.html")
-@app.route("/form", methods =["GET"])
-def home3():
-  return render_template("form.html")
+
 if __name__ == "__main__":
   app.run()
