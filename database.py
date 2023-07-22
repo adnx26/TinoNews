0bdfe83bef25d1fcd89db703119b6490f997494c
Advaith Nair <64707611+adnx26@users.noreply.github.com>
2023-07-22 16:30:43 -0700

    Crated POST and GET API

--- database.py
+++ database.py
@@ -9,16 +9,18 @@ collection = db["Questions"]
 post1 = {"_id": 0, "name": "xob", "score":5}
 post2 = {"_id": 1, "name": "noble", "score":5}
 
-#collection.insert_one(post1)
+##collection.insert_one(post1)
 #collection.insert_many([post1, post2])
 
 
 results = collection.find({"name": "xob", "score":5})
 
+
 for result in results:
     print(result)
     print(result["_id"])
 
-results = collection.find_one({"_id": 0})
+
+results = collection.find_one({"_id": 1})
 
 print(result)
