7926ca7070e3ef3ee46c768e2ad5a7abc8e0a00a
Advaith Nair <64707611+adnx26@users.noreply.github.com>
2023-07-27 12:04:33 -0700

    Finished APIs
    Created APIs for Question, Replies, and Subjects. Deleted old files. Changed how flask is run. Optimized code.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from website import create_app
 from flask_restful import Resource, Api
-
+#Dont need
 app = create_app()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template, jsonify, request
+from flask import Blueprint, render_template, jsonify, request, Flask
 import urllib.request, json
 import os
 import pymongo
 from pymongo import MongoClient
+from datetime import datetime
 
 
-views = Blueprint('views', __name__)
+
+#views = Blueprint('views', __name__)
 cluster = MongoClient("mongodb+srv://TinoTutor:tinotutor1241@tinotutor.dupch6q.mongodb.net/")
 db = cluster["TinoTutor"]
 QDB = db["Questions"] #Question Database
@@ -13,11 +15,14 @@ RDB = db["Replies"] #Replies Database
 UDB = db["User"] #User Database
 SDB = db["Subject"] # Subject Database
 
-#QDB.insert_one({"_id": 0, "uuid": "xob", "time":5, "context":"What is 2+2", "subjectid": "MTH"})
+views = Flask(__name__)
+
+
 
 @views.route('/')
 def home():
-    return render_template("test.html", question = "placeholder")
+    return render_template("home.html", question = "placeholder")
+
 
 #API
 class create_dict(dict): 
@@ -30,20 +35,67 @@ class create_dict(dict):
     def add(self, key, value): 
         self[key] = value
 
-mydict = create_dict()
-for y in QDB.find():
-    mydict.add("Questions", ({"uuid":y['uuid'],"time":y['time'],"context":y['context'], "subjectid":y['subjectid']}))
+
+
+
 #get question
 @views.route('/api/question/get')
 def get_questions():
+    mydict = create_dict()
+    for y in QDB.find():
+        mydict.add(y['_id'], ({"uuid":y['uuid'],"time":y['time'],"context":y['context'], "subjectid":y['subjectid'], "schoolid":y['schoolid']}))
     return json.dumps(mydict)
 
+#post question after enter into input box
 @views.route('/api/question/post', methods=['POST'])
 def add_questions():
     data = request.get_json()
     data = jsonify(data)
 
-    print(request.get_json())
-    QDB.insert_one(str(json.loads(data)))
+    questionDict ={
+        "uuid": 0,
+        "time": str(datetime.now()),
+        "context": request.get_json()["Question"],
+        "subjectid": "CALCAB",
+        "schoolid": 10
+    }
 
+    
+    QDB.insert_one(questionDict)
     return '', 204
+
+
+#get replies
+@views.route('/api/replies/get')
+def get_replies():
+    mydict = create_dict()
+    for y in RDB.find():
+        mydict.add(y['_id'], ({"qid":y['qid'],"uuid":y['uuid'],"time":y['time'], "context":y['context']}))
+    return json.dumps(mydict)
+
+@views.route('/api/replies/post', methods=['POST'])
+def add_replies():
+    data = request.get_json()
+    data = jsonify(data)
+    repliesDict ={
+        "qid": 0,
+        "uuid":0,
+        "time": str(datetime.now()),
+        "context": request.get_json()["Replies"],
+    }
+
+    
+    RDB.insert_one(repliesDict)
+    return '', 204
+
+#get subjects
+@views.route('/api/subjects/get')
+def get_subjects():
+    mydict = create_dict()
+    for y in SDB.find():
+        mydict.add("Subjects", ({"CALCAB":y['CALCAB'],"APLIT":y['APLIT']}))
+    return json.dumps(mydict)
+
+
+if __name__ == '__main__':
+    views.run(debug=True)
